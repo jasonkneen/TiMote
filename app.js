f25afc67a7e85b1a858c1bb2b15f38fb57f8b1fc
Jason Kneen <jason.kneen@bouncingfish.com>
2012-09-12 00:31:59 +0100

    Clean-up / refactor

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
 // create a window
-var win1 = Titanium.UI.createWindow({
+var win = Titanium.UI.createWindow({
 	Title : "TiMote"
 });
-win1.backgroundColor = '#fff';
+
+win.backgroundColor = '#fff';
 
 var tabGroup = Ti.UI.createTabGroup();
 
-var tab1 = Ti.UI.createTab({
+var tab = Ti.UI.createTab({
 	icon : 'KS_nav_views.png',
 	title : 'Code',
-	window : win1
+	window : win
 });
 
-tabGroup.addTab(tab1);
+tabGroup.addTab(tab);
 
 tabGroup.open();
 
-var motecode = require("TiMote/timote");
+var timote = require("TiMote/timote");
 
-motecode.exec('https://raw.github.com/jasonkneen/TiMote/master/remote.js',win1);
+timote.exec('https://raw.github.com/jasonkneen/TiMote/master/remote.js',win);
--- timote.js
+++ timote.js
@@ -3,18 +3,13 @@ exports.exec = function(url,win) {
 	var xhr = Titanium.Network.createHTTPClient();
 	
 	xhr.onerror = function(e) {
-		Ti.API.info('IN ERROR ' + e.error);
+		Ti.API.info('ERROR ' + e.error);
 	};
-	xhr.onload = function(e) {
-		Ti.API.info('EXECUTING CODE' + this.responseText);
-		
+	xhr.onload = function(e) {		
 		eval(this.responseText);
-		
 	};
 	
-	// open the client
 	xhr.open('GET', url);
-	xhr.send();
-	
+	xhr.send();	
 }
 
